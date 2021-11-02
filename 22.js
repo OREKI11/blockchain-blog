79ef9f01cae3788629b7aaebf8003e818299c75e
harshal <harshalmasure5757@gmail.com>
2021-11-03 04:02:29 +0530

    second commit

---
+++ 22.js
@@ -0,0 +1,132 @@
+// function display(){
+//     console.log("hello world");
+
+// }
+//   display();
+
+// var display = function(){
+
+//     console.log("hello wporld ");
+
+// }
+// display();
+
+// var add= function(){
+
+//     var a=3;
+//     var b=4;
+//     var c=a+b;
+//     console.log(c);
+
+// }
+// add();
+
+
+// var a= function( a,b){
+        
+//     c=a+b;
+//     return c;
+
+// }
+// console.log(a(30,50));
+// var add= function(a,b){
+//     var c = a+b;
+//     return c;
+// }
+// var sub = function(c){
+
+//     var  result = add(3,4)-c;
+//     console.log(result);
+// }
+// sub(1);
+
+
+
+// ``````````````````````ARRAY`````````````````
+// var arr= [10,20,30]
+// console.log(arr[1]);
+
+// var arr=[
+//     [10,20,23],["bus","car"]
+// ]
+// console.log(arr[0][1]);
+
+
+//  var num =[10,20,30];
+//  num.push(40)
+//  console.log(num);
+
+
+// `````````````````````Object`````````````````
+
+// var student = {
+//     name : "reena ",
+//     roll: 2,
+
+//     all: true,
+
+//     add: ["rita","kelly"]
+
+
+    
+// }
+// student.age=12
+// console.log(student);
+
+// var lis =[
+
+//     {
+//         name :"krishi",
+//         roll:44,
+//         all:false
+//     },
+//      {
+//          name: "raven",
+//          roll:7,
+//          all:true
+
+
+//     }
+// ];
+//  console.log(lis[0].name);
+
+// ````````````````````````````loops````````````````````
+// var count = 1;
+
+
+// while(count<2){
+
+//     console.log("hello world")
+
+//     count=count+1;
+// }
+
+
+// var count =1;
+//  do{
+
+//     console.log("hello world");
+
+//     count=count+1;
+//  } while(count<1)
+
+// for(i=0;i<5;i++){
+//     console.log("hello world");
+// }
+
+// with array 
+// var arr = ["cat","dog","bad"]
+// // console.log(arr.lenght);
+// for(var i=0;i<<arr.lenght;i++){
+//     console.log(arr[arr]);
+// }
+// var arr = ["cat","dog","bad"]
+// arr.foreach(function(i)
+// {
+//     console.log(i);
+// })
+// var arr = ["cat","dog","bad"]
+// function loop(i){
+//     console.log(i);
+// }
+// arr.forEach(loo
---
+++ index.js
@@ -0,0 +1,8 @@
+   
+    alert("hii");
+
+    var a =  prompt("enter value")
+  if(a>0){
+      alert("it is greater");
+  }
+    
